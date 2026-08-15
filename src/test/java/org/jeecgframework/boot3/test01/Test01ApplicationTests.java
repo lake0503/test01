6ec6c20dbf464936439dcd3b7c4423edfe4a3aaf
lake82 <41097970@qq.com>
2026-08-15 17:41:03 +0800

    Initial commit

---
+++ src/test/java/org/jeecgframework/boot3/test01/Test01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jeecgframework.boot3.test01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
